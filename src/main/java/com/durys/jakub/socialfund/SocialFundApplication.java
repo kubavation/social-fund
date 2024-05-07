b559a73a4e71f2064b8941a18348d394148285ae
kubavation <37585627+kubavation@users.noreply.github.com>
2024-05-07 19:41:50 +0200

    init

---
+++ src/main/java/com/durys/jakub/socialfund/SocialFundApplication.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.socialfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialFundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/durys/jakub/socialfund/SocialFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.socialfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
