ae07c946d75166cb65d0ea46a8914f66cb46c192
kubavation <37585627+kubavation@users.noreply.github.com>
2024-05-09 19:38:01 +0200

    fix

--- src/main/java/com/durys/jakub/socialfund/person/domain/command/PersonCommand.java
+++ src/main/java/com/durys/jakub/socialfund/person/domain/command/PersonCommand.java
@@ -1,11 +1,12 @@
 package com.durys.jakub.socialfund.person.domain.command;
 
+import com.durys.jakub.socialfund.cqrs.Command;
 import com.durys.jakub.socialfund.person.domain.PersonType;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-public sealed interface PersonCommand {
+public sealed interface PersonCommand extends Command {
 
     record ReportPersonCommand(String firstName, String lastName, LocalDate birthdayDate, PersonType type) implements PersonCommand {}
 
