7ace1a51176152cdd3107e0d79d63044d3f4e930
kubavation <37585627+kubavation@users.noreply.github.com>
2024-05-10 20:53:11 +0200

    aggreate root class

--- src/main/java/com/durys/jakub/socialfund/person/domain/Person.java
+++ src/main/java/com/durys/jakub/socialfund/person/domain/Person.java
@@ -1,14 +1,18 @@
 package com.durys.jakub.socialfund.person.domain;
 
+import com.durys.jakub.socialfund.ddd.AggregateRoot;
+import com.durys.jakub.socialfund.ddd.event.DomainEventsPublisher;
+
 import java.time.LocalDate;
 
-public class Person {
+public class Person extends AggregateRoot {
 
     private final PersonId personId;
     private final PersonalData personalData;
     private PersonType type;
 
-    Person(PersonId personId, PersonalData personalData, PersonType type) {
+    Person(DomainEventsPublisher events, PersonId personId, PersonalData personalData, PersonType type) {
+        super(events);
         this.personId = personId;
         this.personalData = personalData;
         this.type = type;
