95ea9769edb5fda8e2170651b11909d617164301
kubavation <37585627+kubavation@users.noreply.github.com>
2024-06-12 20:50:30 +0200

    declarations spring configuration

--- src/main/java/com/durys/jakub/socialfund/declarations/infrastructure/InMemoryIncomeGroupResolver.java
+++ src/main/java/com/durys/jakub/socialfund/declarations/infrastructure/InMemoryIncomeGroupResolver.java
@@ -1,11 +1,9 @@
 package com.durys.jakub.socialfund.declarations.infrastructure;
 
 import com.durys.jakub.socialfund.declarations.domain.IncomeGroupResolver;
-import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-@Component
 public class InMemoryIncomeGroupResolver implements IncomeGroupResolver {
 
     @Override
--- src/main/java/com/durys/jakub/socialfund/declarations/infrastructure/SpringIncomeDeclarationFactory.java
+++ src/main/java/com/durys/jakub/socialfund/declarations/infrastructure/SpringIncomeDeclarationFactory.java
@@ -6,13 +6,11 @@ import com.durys.jakub.socialfund.declarations.domain.IncomeDeclaration;
 import com.durys.jakub.socialfund.declarations.domain.IncomeDeclarationFactory;
 import com.durys.jakub.socialfund.declarations.domain.IncomeGroupResolver;
 import com.durys.jakub.socialfund.person.domain.PersonId;
-import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Component
 public class SpringIncomeDeclarationFactory implements IncomeDeclarationFactory {
 
     private final DomainEventsPublisher eventsPublisher;
