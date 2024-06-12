ca7d1ef3676a3eda006b42534c4eb6c911822b2c
kubavation <37585627+kubavation@users.noreply.github.com>
2024-06-12 20:25:40 +0200

    fixes + initial version of incomeDeclarationFactory

--- src/main/java/com/durys/jakub/socialfund/declarations/domain/IncomeDeclaration.java
+++ src/main/java/com/durys/jakub/socialfund/declarations/domain/IncomeDeclaration.java
@@ -1,31 +1,61 @@
 package com.durys.jakub.socialfund.declarations.domain;
 
+import com.durys.jakub.socialfund.common.DomainValidationException;
 import com.durys.jakub.socialfund.ddd.AggregateRoot;
 import com.durys.jakub.socialfund.ddd.event.DomainEventsPublisher;
 import com.durys.jakub.socialfund.person.domain.PersonId;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class IncomeDeclaration extends AggregateRoot {
+
+    public enum State {
+        Draft, Submitted, Accepted, Rejected
+    }
 
     private final IncomeGroupResolver incomeGroupResolver;
 
     private final DeclarationId id;
     private final PersonId personId;
     private final LocalDateTime submittedAt;
-    private final Integer version;
+    private Integer version;
     private final Integer year;
-    private final Integer incomeGroup;
+    private Integer incomeGroup;
+    private BigDecimal income;
+    private State state;
 
     IncomeDeclaration(DomainEventsPublisher events, IncomeGroupResolver incomeGroupResolver,
-                      DeclarationId id, PersonId personId, LocalDateTime submittedAt, Integer version, Integer year, Integer incomeGroup) {
+                      DeclarationId id, PersonId personId, LocalDateTime submittedAt, Integer year, BigDecimal income) {
         super(events);
         this.id = id;
         this.personId = personId;
         this.submittedAt = submittedAt;
-        this.version = version;
         this.year = year;
-        this.incomeGroup = incomeGroup;
+        this.income = income;
         this.incomeGroupResolver = incomeGroupResolver;
     }
+
+    public void saveDraft() {
+
+        if (state != State.Draft) {
+            throw new DomainValidationException("Invalid state for this operation");
+        }
+
+        incomeGroup = incomeGroupResolver.resolve(year, income);
+
+        //todo event
+    }
+
+    public void submit() {
+
+        if (state != State.Draft) {
+            throw new DomainValidationException("Invalid state for this operation");
+        }
+
+        state = State.Submitted;
+
+        //todo event
+    }
+
 }
--- src/main/java/com/durys/jakub/socialfund/declarations/domain/IncomeGroupResolver.java
+++ src/main/java/com/durys/jakub/socialfund/declarations/domain/IncomeGroupResolver.java
@@ -3,5 +3,5 @@ package com.durys.jakub.socialfund.declarations.domain;
 import java.math.BigDecimal;
 
 public interface IncomeGroupResolver {
-    Integer resolve(Long year, BigDecimal income);
+    Integer resolve(Integer year, BigDecimal income);
 }
